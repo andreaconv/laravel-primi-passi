a0d350c092689280e9edf7022ff6db40d2510e78
andreaconv <andrea2convertini@gmail.com>
2023-06-01 19:25:20 +0200

    [add] hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = 'Ciccillo';
+    $lastname = 'Cacace';
+    return view('homepage', compact('name', 'lastname'));
 });
